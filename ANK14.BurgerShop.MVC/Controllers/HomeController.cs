c6196c2cff6a3855774bfd9c9c808741c6096bc0
cllkbc <burcuclk2020@hotmail.com>
2023-12-08 07:17:17 +0300

    some pages reorganized

--- ANK14.BurgerShop.MVC/Controllers/HomeController.cs
+++ ANK14.BurgerShop.MVC/Controllers/HomeController.cs
@@ -18,6 +18,10 @@ namespace ANK14.BurgerShop.MVC.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
